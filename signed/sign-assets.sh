c413054a7d80f816dd0cf675ca270a0d455c1235
Matt Borja <3855027+mattborja@users.noreply.github.com>
2022-06-10 08:09:21 -0700

    Update sign-assets.sh
    - Remove *.sh file exclusion (includes sign-assets.sh)
    - Sort file listing
    - Output

--- signed/sign-assets.sh
+++ signed/sign-assets.sh
@@ -6,5 +6,6 @@ PGP_KEY="0x33688C2EDC08CD38"
 find . -type f \
        -not -name ".DS_Store" \
        -not -name "*.sig" \
-       -not -name "*.sh" \
-       | gpg --clearsign --armor --local-user "$PGP_KEY" > MANIFESTSUM.sig
+       | sort | gpg --clearsign --armor --local-user "$PGP_KEY" > MANIFESTSUM.sig
+
+cat MANIFESTSUM.sig
