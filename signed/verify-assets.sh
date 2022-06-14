1f8de14523b670155c6fad267822911d72a3650b
Matt Borja <3855027+mattborja@users.noreply.github.com>
2022-06-14 00:09:19 -0700

    Remove deprecated copy of promoted files
    Signed-off-by: Matt Borja <3855027+mattborja@users.noreply.github.com>

--- signed/verify-assets.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Verifies origin of MANIFESTSUM.sig used in subsequent SHA2 verification of tracked files
-
-MANIFESTSUM_OUTPUT="./MANIFESTSUM.sig"
-
-gpg -d "$MANIFESTSUM_OUTPUT" | shasum -c
