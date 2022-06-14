f118a8dcacb70338eb6a4ef8466b806be4b7650f
Matt Borja <3855027+mattborja@users.noreply.github.com>
2022-06-13 21:58:17 -0700

    Include SHA2 hashing in sign-assets.sh and rebuild
    Also cleans up current asset filenames now shown in MANIFESTSUM.sig with their corresponding checksums

--- signed/sign-assets.sh
+++ signed/sign-assets.sh
@@ -2,10 +2,15 @@
 # Rebuilds MANIFESTSUM.sig from local directory using $PGP_KEY
 
 PGP_KEY="0x33688C2EDC08CD38"
+MANIFESTSUM_OUTPUT="./MANIFESTSUM.sig"
 
 find . -type f \
        -not -name ".DS_Store" \
        -not -name "*.sig" \
-       | sort | gpg --clearsign --armor --local-user "$PGP_KEY" > MANIFESTSUM.sig
+       | sort \
+       | xargs shasum -a 256 \
+       | gpg --clearsign --armor --local-user "$PGP_KEY" > "$MANIFESTSUM_OUTPUT"
 
-cat MANIFESTSUM.sig
+cat "$MANIFESTSUM_OUTPUT"
+
+echo "To verify, type: gpg -d "$MANIFESTSUM_OUTPUT" | shasum -c"
